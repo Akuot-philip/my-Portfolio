adc593fd79ab3443f0c83d87b2d8a28e7884b1ad
Akuot-philip <akuotphilipp@gmail.com>
2022-06-29 08:55:57 +0300

    my porfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+/*==============================typing animation===================*/
+var types = new Typed(".typing",{
+    strings:["","Web Developer", "UI/UX designer","Front Web Mobile Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
